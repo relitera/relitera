44a39a554e8fc993e325447de790fe103ed7bc2b
caique-a <227167826+caique-a@users.noreply.github.com>
2025-09-18 16:52:50 -0300

    feat: adiciona backdrop para a sidebar com transições de visibilidade

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const sidebar = document.querySelector(".sidebar");
     const sidebarToggle = document.querySelector(".fa-bars");
     const sidebarClose = document.querySelector(".sidebar-close");
+    const backdrop = document.querySelector(".sidebar-backdrop");
 
     function toggleSidebar() {
         sidebar.classList.toggle("open");
+        backdrop.classList.toggle("active");
     }
 
     function closeSidebar() {
         sidebar.classList.remove("open");
+        backdrop.classList.remove("active");
     }
 
     sidebarToggle.addEventListener("click", toggleSidebar);
     sidebarClose.addEventListener("click", closeSidebar);
+    backdrop.addEventListener("click", closeSidebar);
 });
