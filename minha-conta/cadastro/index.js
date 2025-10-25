23397008727cdefaebfcde506e14cd48409cd096
ivespf12 <ives@koalitytech.com>
2025-10-25 12:58:23 -0300

    Implementando registro

--- minha-conta/cadastro/index.js
+++ minha-conta/cadastro/index.js
@@ -1,63 +1,107 @@
+import userStore from "../../store/UserStore/UserStore.js";
 const botaoApagar = document.getElementById("apagar");
 const botaoRegistrar = document.getElementById("registrar");
-const nome = document.getElementById('nomeUsuario');
-const data = document.getElementById('nascimentoUsuario');
-const email = document.getElementById('emailUsuario');
-const senha = document.getElementById('passwordUsuario');
-const alerta = document.getElementById('alerta');
+const nome = document.getElementById("nomeUsuario");
+const data = document.getElementById("nascimentoUsuario");
+const email = document.getElementById("emailUsuario");
+const senha = document.getElementById("passwordUsuario");
+const alerta = document.getElementById("alerta");
 
-email.addEventListener('focus', () => {
-    email.classList.remove('ok');
-    email.classList.remove('erro');
-    email.classList.add('averiguando');
-})
+const getFormValues = () => {
+    const nome = document.getElementById("nomeUsuario").value;
+    const data = document.getElementById("nascimentoUsuario").value;
+    const email = document.getElementById("emailUsuario").value;
+    const senha = document.getElementById("passwordUsuario").value;
 
-email.addEventListener('blur', () => {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
-    email.classList.remove('averiguando');
-
-    const valido = regex.test(email.value.trim());
-    email.classList.toggle("erro", !valido);
-    email.classList.toggle("ok", valido);
-    email.setAttribute("aria-invalid", String(!valido));
-    
-    botaoRegistrar.disabled = !valido;
-})
-
-senha.addEventListener('focus', () => {
-    alerta.innerHTML = 'A senha deve possuir pelo menos 2 números, uma letra maiúscula e um caractere especial.';
-    alerta.classList.add('alerta')
-})
-
-senha.addEventListener('blur', () => {
-    alerta.innerHTML = '';
-    alerta.classList.remove('alerta');
-})
-
-botaoRegistrar.addEventListener('click', function() {
-
-    if((nome.value === '')||(data.value === '')||(email.value === '')||(senha.value === '')) {
-        alert("Preencha todos requisitos");
-    } else {
-        const nomeUsuario = nome.value;
-        const dataUsuario = data.value;
-        const emailUsuario = email.value;
-        const senhaUsuario = senha.value;
-
-        console.log(nomeUsuario);
-        console.log(dataUsuario);
-        console.log(emailUsuario);
-        console.log(senhaUsuario);
-        apagar();
+    return {
+        name: nome,
+        birthdate: data,
+        email,
+        password: senha
     }
-})
+}
 
-botaoApagar.addEventListener('click', apagar);
+email.addEventListener("focus", () => {
+  email.classList.remove("ok");
+  email.classList.remove("erro");
+  email.classList.add("averiguando");
+});
+
+email.addEventListener("blur", () => {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+  email.classList.remove("averiguando");
+
+  const valido = regex.test(email.value.trim());
+  email.classList.toggle("erro", !valido);
+  email.classList.toggle("ok", valido);
+  email.setAttribute("aria-invalid", String(!valido));
+
+  botaoRegistrar.disabled = !valido;
+});
+
+senha.addEventListener("focus", () => {
+  alerta.innerHTML =
+    "A senha deve possuir pelo menos 2 números, uma letra maiúscula e um caractere especial.";
+  alerta.classList.add("alerta");
+});
+
+senha.addEventListener("blur", () => {
+  alerta.innerHTML = "";
+  alerta.classList.remove("alerta");
+});
+console.log(botaoRegistrar)
+botaoRegistrar.addEventListener("click", async function () {
+  try {
+    console.log("registrando");
+
+    const formData = getFormValues();
+
+    console.log(formData)
+
+    const newUser = await fetch("http://localhost:8000/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const newUserRes = await newUser.json();
+
+    if (!newUserRes) return
+    console.log(newUserRes);
+    if (newUserRes.statusCode === 400) {
+        alerta.innerHTML = newUserRes.client_message;
+        alerta.classList.remove("alerta");
+    }
+
+    if (newUserRes.id) {
+        userStore.setUser(newUserRes)
+
+        localStorage.setItem("user-token", newUserRes.token)
+    
+        alerta.innerHTML = "Conta criada com sucesso, aguarde enquanto te redirecionamos na página principal";
+        alerta.classList.remove("alerta");
+
+        setTimeout(() => {
+            window.location.href = "/"
+        }, 1000)
+    }
+
+    console.log(userStore.user)
+  } catch (err) {
+    alerta.innerHTML = "Ocorreu um erro. Tente novamente mais tarde";
+    alerta.classList.remove("alerta");
+    console.error(err);
+  }
+});
+
+botaoApagar.addEventListener("click", apagar);
 
 function apagar() {
-
-    nome.value = '';
-    data.value = '';
-    email.value = '';
-    senha.value = '';
+  nome.value = "";
+  data.value = "";
+  email.value = "";
+  senha.value = "";
 }
---
+++ store/UserStore/UserStore.js
@@ -0,0 +1,29 @@
+class UserStore {
+  constructor() {
+    this.user = null;
+    this.loadFromStorage();
+  }
+
+  setUser(user) {
+    this.user = user;
+    localStorage.setItem("user", JSON.stringify(user));
+  }
+
+  getUser() {
+    return this.user;
+  }
+
+  clearUser() {
+    this.user = null;
+    localStorage.removeItem("user");
+  }
+
+  loadFromStorage() {
+    const saved = localStorage.getItem("user");
+    if (saved) this.user = JSON.parse(saved);
+  }
+}
+
+const userStore = new UserStore()
+
+export default userStore
