b52a960703ca7c006d01d9ee8efac1a90c2cb9ae
caique-a <227167826+caique-a@users.noreply.github.com>
2025-11-21 13:19:41 -0300

    fix: botaoSair carrega antes da pagina

--- minha-conta/perfil/index.js
+++ minha-conta/perfil/index.js
@@ -1,10 +1,14 @@
 import userStore from "../../store/UserStore/UserStore.js";
 
-const botaoSair = document.getElementById("botaoSair");
+document.addEventListener("DOMContentLoaded", () => {
+    const botaoSair = document.getElementById("botaoSair");
 
-botaoSair.addEventListener("click", () => {
-    localStorage.removeItem("user-token");
-    localStorage.removeItem("user");
-    userStore.clearUser();
-    window.location.href = "/minha-conta/login/";
-})
+    if (botaoSair) {
+        botaoSair.addEventListener("click", () => {
+            localStorage.removeItem("user-token");
+            localStorage.removeItem("user");
+            userStore.clearUser();
+            window.location.href = "/minha-conta/login/";
+        });
+    }
+});
