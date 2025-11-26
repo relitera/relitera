19eae5d756158edce3b6da06a8ce4b16d66ceb84
ives <ivespf123@gmail.com>
2025-11-26 08:43:09 -0300

    Dados do usuário

---
+++ minha-conta/dados/index.js
@@ -0,0 +1,31 @@
+import userStore from "../../store/UserStore/UserStore.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const nomeUsuarioText = document.getElementById("nome-usuario")
+    const emailUsuarioText = document.getElementById("email-usuario")
+    const dataDeNascimentoText = document.getElementById("data-de-nascimento")
+
+    const usuario = userStore.user
+
+    if (!usuario) return
+
+    if (usuario.name) {
+        nomeUsuarioText.textContent = `Nome: ${usuario.name}`
+    }
+
+    if (usuario.email) {
+        emailUsuarioText.textContent = `Email: ${usuario.email}`
+    }
+
+    if (usuario.birthdate) {
+        const date = new Date(usuario.birthdate)
+
+        let day = date.getDay()
+
+        if (parseInt(day) < 10) {
+            day = '0' + day
+        }
+        console.log(date)
+        dataDeNascimentoText.textContent = `Data de nascimento: ${day}-${date.getMonth()}-${date.getFullYear()}`
+    }
+});
